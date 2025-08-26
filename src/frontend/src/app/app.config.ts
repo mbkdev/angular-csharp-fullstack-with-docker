bfefa8380343aaf05b2aea290364f09daca5385d
Max <mbk_dev@icloud.com>
2025-08-26 12:07:11 +0200

    Added possibility to test in debug-mode (different environments)

--- src/frontend/src/app/app.config.ts
+++ src/frontend/src/app/app.config.ts
@@ -1,16 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { jwtInterceptor } from './authentication/jwt.interceptor';
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([jwtInterceptor]))
+    provideRouter(routes)
   ]
 };
--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,13 +1,20 @@
 import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { App } from './app/app';
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { API_BASE_URL } from './app/tokens';
+import { WeatherBackendModel } from './app/models/weatherBackendModel';
 
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(BrowserModule, HttpClientModule), 
+    importProvidersFrom(BrowserModule, HttpClientModule),
+    {
+      provide: WeatherBackendModel,
+      useFactory: (http: HttpClient, apiBaseUrl: string) => new WeatherBackendModel(http, apiBaseUrl),
+      deps: [HttpClient, API_BASE_URL]
+    }
   ]
 });
